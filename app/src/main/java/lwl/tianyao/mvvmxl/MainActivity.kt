655a214dff0e3f0ac64edcd3609bb8daae7973bf
tianyao106 <7803406+tianyao106@user.noreply.gitee.com>
2022-10-01 17:53:16 +0800

    1、app添加

---
+++ app/src/main/java/lwl/tianyao/mvvmxl/MainActivity.kt
@@ -0,0 +1,45 @@
+package lwl.tianyao.mvvmxl
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alibaba.android.arouter.facade.annotation.Route
+import lwl.tianyao.mvvmxl.ui.theme.MvvmXlTheme
+
+@Route(path = "/main/MainActivity")
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MvvmXlTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello22222 $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MvvmXlTheme {
+        Greeting("Android")
+    }
+}
